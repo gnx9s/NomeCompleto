39920b83b9b11b83d3afacea5e8d59190d30eb62
gnx9s <gabriel.rodriguesm0rg@outlook.com>
2023-04-01 03:58:24 -0300

    exercicio pronto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string nome;
+
+
+Console.WriteLine("Qual é o seu nome? ");
+nome = Console.ReadLine()!;
+
+
+Console.WriteLine("Qual é o seu sobrenome? ");
+string sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Olá, {nome} {sobrenome}!");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
